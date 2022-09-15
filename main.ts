cd178d8ee26592d7df36faee92f1a54aa4637336
Danie-u123 <113040612+Danie-u123@users.noreply.github.com>
2022-09-15 10:05:13 -0400

    Update pxt.json, main.blocks, main.ts, main.py

--- main.ts
+++ main.ts
@@ -1,16 +1,12 @@
 basic.forever(function () {
     basic.showLeds(`
         . # . # .
-        # # . # #
-        # # . # #
-        . . . . .
+        # # # # #
+        # # # # #
+        . # # # .
         . . # . .
         `)
-    basic.showLeds(`
-        . . . . .
-        . . . . .
-        . . . . .
-        . . . . .
-        . . . . .
-        `)
+    basic.showIcon(IconNames.Skull)
+    basic.showString("LETSS GOOO ;)")
+    music.playMelody("E A D G B F C5 C ", 185)
 })
